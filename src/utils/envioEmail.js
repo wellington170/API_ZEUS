b369303b0f286c37c0fd6986ebc18a3370b612c8
Wellington Costa <wellingtoncostagrilo2018@gmail.com>
2025-05-19 22:06:20 -0300

    fix: Melhoria no texto no envio por email

--- src/utils/envioEmail.js
+++ src/utils/envioEmail.js
@@ -5,10 +5,20 @@ const transporter = nodemailer.createTransport(mailConfig);
 
 async function enviaCodigo(email, code) {
   await transporter.sendMail({
-    from: '"Comp Junior "<wellingtoncostagrilo2018@gmail.com>',
+    from: '"Equipe de suporte - Comp Júnior "<wellingtoncostagrilo2018@gmail.com>',
     to: email,
-    subject: 'Recuperação de senha:',
-    text: `Seu código de recuperação ${code} expira em 5 minutos!`,
+    subject: 'Recuperação de senha-Comp Júnior',
+    text: `Olá,
+    Recebemos uma solicitação para redefinir a sua senha.
+    Utilize o código abaixo para prosseguir com a recuperação da sua conta:
+
+    Código de recuperação: ${code}
+
+    Este código é válido por apenas 5 minutos. 
+    Se você não solicitou essa alteração, ignore este e-mail.
+
+    Atenciosamente,
+    Equipe de Suporte - Comp Júnior`
   });
 }
 
