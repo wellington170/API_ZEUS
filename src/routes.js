ee5b10447a1f8de6fceeca746125abaa7bd3196f
Wellington Costa <wellingtoncostagrilo2018@gmail.com>
2025-05-19 22:10:48 -0300

    feat: adição das rotas de controle dos membros

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 const {Router}=require('express');
-const UserModel=require('./apps/models/membros');
 const AuthenticationMiddleware=require('./apps/middlewares/authentication');
 const ControleDeMembros = require('./apps/controllers/ControleDeMembros');
 const AuthenticationController=require('./apps/controllers/Autenticação');
 const ControleOrcamentos=require('./apps/controllers/ControleOrcamentos');
+const ControleDosMembros=require('./apps/controllers/ControleDosMembros');
 
 const schemaValidator=require('./apps/middlewares/schemaValidator');
 const membroSchema=require('./schema/create_membro.json');
@@ -14,17 +14,22 @@ const resetSchema=require('./schema/recuperacao_senha_schema.json');
 const codigoSchema=require('./schema/envio_codigo_shema.json');
 const createOrcamentoSchema=require('./schema/create_orcamento.json');
 const updateOrcamentosSchema=require('./schema/update_orcamento.json');
+const updatePerfilSchema=require('./schema/update_perfil.json');
+const updateOrcamentoUserSchema=require('./schema/update_orcamento_user.json');
 const routes= new Router();
 const {upload, verificaErroMulter}=require('./configs/multer');
+
 routes.post('/login', schemaValidator(loginSchema),AuthenticationController.authenticate);
 routes.post('/primeiro_login', schemaValidator(firstLoginSchema), AuthenticationController.fistAuthenticate);
 routes.post('/login/recuperacao_de_senha/codigo', schemaValidator(codigoSchema), AuthenticationController.codigo);
 routes.put('/login/recuperacao_de_senha/reset', schemaValidator(resetSchema), AuthenticationController.reset);
 
 routes.use(AuthenticationMiddleware);
-routes.get('/health', (req,res)=>{
-    return res.send({message: "Connection is ok!"});
-})
+
+routes.put('/user/perfil', upload.single('foto'),
+schemaValidator(updatePerfilSchema),verificaErroMulter, ControleDosMembros.updateMembro);
+routes.get('/user/listarOrcamentos', ControleDosMembros.listar);
+routes.put('/user/updateOrcamento/:id', schemaValidator(updateOrcamentoUserSchema), ControleDosMembros.updateOrcamento);
 
 routes.post('/adm/create',upload.single('foto'),
 schemaValidator(membroSchema),verificaErroMulter, ControleDeMembros.create);
@@ -37,4 +42,5 @@ routes.post('/adm/orcamento/create',schemaValidator(createOrcamentoSchema), Cont
 routes.delete('/adm/orcamento/delete/:id', ControleOrcamentos.delete);
 routes.put('/adm/orcamento/update/:id',schemaValidator(updateOrcamentosSchema), ControleOrcamentos.update);
 routes.get('/adm/orcamento/listar', ControleOrcamentos.listar);
+
 module.exports=routes;
